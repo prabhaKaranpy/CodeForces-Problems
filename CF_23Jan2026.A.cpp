7ffe4d3a51a75aba1b3267b44552ed27abfb9910
prabhaKaranpy <prabhakaran80483@gmail.com>
2026-01-27 01:20:30 +0530

    solved 23rd Jan 2026 Div.2 A

--- CF_23Jan2026.A.cpp
+++ CF_23Jan2026.A.cpp
@@ -12,25 +12,45 @@ signed main(void){
     while(T--){
         int n, h, l; cin>>n>>h>>l; 
         vector<int> arr(n); for(int &it : arr) cin>>it; 
-        map<int, int> mp; for(auto &it : arr) mp[it] ++; 
-        int count = 0, prev = 0; 
-        while(1){ 
-            for(int i = 1; i <= h; i++){
-                for(int j = 1; j <= l; j++){
-                    if(i != j && mp[i] > 0 && mp[j] > 0){
-                        mp[i] --; mp[j] --; 
-                        count ++; 
-                    }
-                    else{
-                        if(mp[i] > 1){
-                            mp[i] -= 2; count ++; 
-                        }
-                    }
-                }
+        sort(arr.begin(), arr.end()); 
+        int x, y; x = y = n; 
+        for(int i = 0; i < n; i++){
+            if(arr[i] > h){
+                x = i; break; 
             } 
-            if(count == prev) break; 
-            prev = count; 
-        }
+        } 
+        for(int i =0; i < n; i++){
+            if(arr[i] > l){
+                y = i; break;  
+            } 
+        } 
+        int maxi = max(x, y), mini = min(x, y); 
+        int rem = maxi-mini; 
+        int count = min(rem, mini); 
+        if(mini-rem > 0){
+            count += ((mini-rem)/2LL); 
+        } 
         cout<<count<<endl; 
+        // cout<<"X : "<<x<<" Y : "<<y<<endl; 
+        // map<int, int> mp; for(auto &it : arr) mp[it] ++; 
+        // int count = 0, prev = 0; 
+        // while(1){ 
+        //     for(int i = 1; i <= h; i++){
+        //         for(int j = 1; j <= l; j++){
+        //             if(i != j && mp[i] > 0 && mp[j] > 0){
+        //                 mp[i] --; mp[j] --; 
+        //                 count ++; 
+        //             }
+        //             else{
+        //                 if(mp[i] > 1){
+        //                     mp[i] -= 2; count ++; 
+        //                 }
+        //             }
+        //         }
+        //     } 
+        //     if(count == prev) break; 
+        //     prev = count; 
+        // }
+        // cout<<count<<endl; 
     }
 }
